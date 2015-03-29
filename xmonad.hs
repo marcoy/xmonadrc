2b8f25df8928c5a06174863c8ac15ecad31b53ec
Marco Yuen <marcoy@gmail.com>
2015-03-29 16:19:58 -0400

    ManageHook and key bindings.

--- xmonad.hs
+++ xmonad.hs
@@ -3,10 +3,12 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.EwmhDesktops
-import XMonad.Layout.Tabbed
+import XMonad.Layout.Maximize
 import XMonad.Layout.ResizableTile
+import XMonad.Layout.Tabbed
 import XMonad.Util.Run(spawnPipe, safeSpawn)
 import XMonad.Util.EZConfig (additionalKeys)
+import qualified XMonad.StackSet as W
 
 import qualified Data.Map as M
 
@@ -14,12 +16,12 @@ myModMask  = mod1Mask -- mod4Mask
 myTerminal = "lilyterm"
 myBrowser  = "chromium"
 
-myManageHook = composeAll
-    [ className =? "Gimp"      --> doFloat
-    , className =? "Vncviewer" --> doFloat
-    , className =? "Vlc"       --> doFloat
-    , isFullscreen             --> doFullFloat
-    ]
+myManageHook = composeAll . concat $
+        [ [className =? f  --> doFloat      | f <- myFloats]
+        , [isFullscreen    --> doFullFloat                 ]
+        ]
+        where
+            myFloats = ["Vlc", "Vncviewer"]
 
 main :: IO ()
 main = xmonad =<< statusBar barCmd pp toggleStrutsKey conf
@@ -48,16 +50,18 @@ myConfig = ewmh defaultConfig { borderWidth = 1
 
 -- Layout
 -- Use `onWorkspaces' to configure layout on different workspaces.
-myLayout = tiled ||| Mirror tiled ||| simpleTabbed ||| Full
+myLayout = maximize tiled ||| Mirror tiled ||| tabbed shrinkText defaultTheme ||| Full
     where
         tiled = ResizableTall 1 (3/100) (1/2) []
 
 -- Keys
 toggleStrutsKey' XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
 
-myKeys = [ ((myModMask               , xK_space ), sendMessage NextLayout)
-         , ((myModMask               , xK_Return), spawn $ XMonad.terminal myConfig)
-         , ((myModMask .|. shiftMask , xK_Return), safeSpawn myBrowser [])
-         , ((myModMask               , xK_a     ), sendMessage MirrorExpand)
-         , ((myModMask               , xK_z     ), sendMessage MirrorShrink)
+myKeys = [ ((myModMask                 , xK_space    ), sendMessage NextLayout)
+         , ((myModMask                 , xK_Return   ), spawn $ XMonad.terminal myConfig)
+         , ((myModMask .|. shiftMask   , xK_Return   ), safeSpawn myBrowser [])
+         , ((myModMask .|. controlMask , xK_Return   ), windows W.swapMaster)
+         , ((myModMask                 , xK_a        ), sendMessage MirrorExpand)
+         , ((myModMask                 , xK_z        ), sendMessage MirrorShrink)
+         , ((myModMask                 , xK_backslash), withFocused (sendMessage . maximizeRestore))
          ]
