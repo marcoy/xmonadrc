1858544bd8979587893bfe34a37de94944cde6a0
Marco Yuen <marcoy@gmail.com>
2015-04-04 19:28:45 -0400

    Tag windows, and set background.

--- xmonad.hs
+++ xmonad.hs
@@ -1,5 +1,7 @@
 import XMonad
 
+import XMonad.Actions.TagWindows
+
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
@@ -10,12 +12,12 @@ import XMonad.Layout.ResizableTile
 import XMonad.Layout.Tabbed
 import XMonad.Layout.ToggleLayouts
 
+import XMonad.Prompt
+
 import XMonad.Util.Run(spawnPipe, safeSpawn)
 import XMonad.Util.EZConfig (additionalKeys)
 
 import qualified XMonad.StackSet as W
-
-
 import qualified Data.Map as M
 
 myModMask  = mod4Mask -- mod1Mask
@@ -37,6 +39,15 @@ main = xmonad =<< statusBar barCmd pp toggleStrutsKey conf
             conf   = myConfig
             toggleStrutsKey = toggleStrutsKey'
 
+myConfig = ewmh defaultConfig { borderWidth = 1
+                              , modMask     = myModMask
+                              , terminal    = myTerminal
+                              , manageHook  = manageDocks <+> myManageHook <+> manageHook defaultConfig
+                              , layoutHook  = avoidStruts  $  myLayout
+                              , handleEventHook = handleEventHook defaultConfig <+> fullscreenEventHook
+                              } `additionalKeys` myKeys
+
+-- PrettyPrinting for xmobar
 myPP = defaultPP { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">"
                  , ppHidden = xmobarColor "#C98F0A" ""
                  , ppHiddenNoWindows = xmobarColor "#C9A34E" ""
@@ -46,13 +57,7 @@ myPP = defaultPP { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">"
                  , ppSep = xmobarColor "#429942" "" " | "
                  }
 
-myConfig = ewmh defaultConfig { borderWidth = 1
-                              , modMask     = myModMask
-                              , terminal    = myTerminal
-                              , manageHook  = manageDocks <+> myManageHook <+> manageHook defaultConfig
-                              , layoutHook  = avoidStruts  $  myLayout
-                              , handleEventHook = handleEventHook defaultConfig <+> fullscreenEventHook
-                              } `additionalKeys` myKeys
+myXPConfig = defaultXPConfig { position = Top }
 
 -- Layout
 -- Use `onWorkspaces' to configure layout on different workspaces.
@@ -71,4 +76,7 @@ myKeys = [ ((myModMask                 , xK_space    ), sendMessage NextLayout)
          , ((myModMask                 , xK_a        ), sendMessage MirrorExpand)
          , ((myModMask                 , xK_z        ), sendMessage MirrorShrink)
          , ((myModMask                 , xK_backslash), withFocused (sendMessage . maximizeRestore))
+         , ((myModMask                 , xK_g        ), tagPrompt myXPConfig (\s -> focusUpTaggedGlobal s))
+         , ((myModMask .|. controlMask , xK_g        ), tagDelPrompt myXPConfig)
+         , ((myModMask .|. shiftMask   , xK_g        ), tagPrompt myXPConfig (\s -> withFocused (addTag s)))
          ]
